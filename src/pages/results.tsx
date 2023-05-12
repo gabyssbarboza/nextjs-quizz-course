5145a99b5b4137d4839e9103f25cfc9f2ebfaa1f
gabyssbarboza <gabrielly.barboza@outlook.com>
2023-05-12 18:35:17 +0100

    fix: results router value

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -7,33 +7,17 @@ import Button from "@/components/Button";
 const Results = () => {
   const router = useRouter();
 
-  React.useEffect(() => {
-    if (router.isReady) {
-      // Code using query
-      console.log(router.query);
-    }
-  }, [router.isReady]);
-
-  const total = +router.query.total;
-  const corrects = +router.query.corrects;
-  const porcent = Math.round((corrects / total) * 100);
+  const total = router.query.total;
+  const corrects = router.query.corrects;
+  const porcent = Math.round((Number(corrects) / Number(total)) * 100);
 
   return (
     <div className={styles.resultsContainer}>
       <h1>Resultado Final</h1>
       <div style={{ display: "flex" }}>
-        {router && (
-          <>
-            {" "}
-            <Statistics value={total} text="Perguntas" />
-            <Statistics value={corrects} text="Certas" bgColor="#9cd2a4" />
-            <Statistics
-              value={`${porcent}%`}
-              text="Percentual"
-              bgColor="#DE6A33"
-            />
-          </>
-        )}
+        <Statistics value={total} text="Perguntas" />
+        <Statistics value={corrects} text="Certas" bgColor="#9cd2a4" />
+        <Statistics value={`${porcent}%`} text="Percentual" bgColor="#DE6A33" />
       </div>
       <Button href="/" txt="Tentar Novamente" />
     </div>
